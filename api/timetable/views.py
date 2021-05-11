9b17a11b6aa6a5b3b55d3fd59f289c6d64d438c6
NickPYTH <nicksrnk@gmail.com>
2021-05-11 10:55:31 +0500

    added docker

---
+++ api/timetable/views.py
@@ -0,0 +1,25 @@
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.response import Response
+from .serializers import TimetableSerializer
+from .models import Timetable
+
+class CreateTimetableView(generics.CreateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = TimetableSerializer
+
+class GetAllUserTimetablesList(generics.ListCreateAPIView):
+    queryset = Timetable.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = TimetableSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = TimetableSerializer(queryset.all(), many=True)
+        return Response(serializer.data)
+
+class UpdateUserTimetableView(generics.UpdateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = TimetableSerializer
--- webcalendar/timetable/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
- 
-class CaseForm(forms.Form):
-    case_start = forms.TimeField()
-    case_end = forms.TimeField()
-    case = forms.CharField(max_length=50)
-    case_description = forms.CharField(required=False)
