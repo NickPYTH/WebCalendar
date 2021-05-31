93e476e17ce922cc448ba1a0c5c1c5cf7738ef80
NickPYTH <nicksrnk@gmail.com>
2021-05-31 20:36:20 +0300

    fix dock

--- api/api/settings.py
+++ api/api/settings.py
@@ -145,12 +145,8 @@ USE_TZ = True
 
 
 STATIC_URL = "/static/"
-#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/mediafiles/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
-STATIC_ROOT = os.path.dirname(os.path.realpath(__file__))
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR , 'static/'),
-)
